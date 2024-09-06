ffdfb1ceeba273efadc4a8d777cdde9e97f04bb6
Yousuke7052 <130848000+Yousuke7052@users.noreply.github.com>
2024-09-06 23:30:17 +0800

    Create hello.py
    Create hello.py

---
+++ hello.py
@@ -0,0 +1,15 @@
+# hello.py
+def main():
+    print("------Hello, world!------")
+    print(">>Instruction completed<<")
+    print(".------------------------")
+    print("|  .---------------------")
+    print("|  |  .------------------")
+    print("|  |  |  .---------------")
+    print("|  |  |  |               ")
+    print("--------Building!--------")
+    print("-------------------------")
+    print('Hello, this is an updated message!')
+
+if __name__ == "__main__":
+    main()
